f37c7487afaa75f9ed28f882f1115dbb35158b7e
PERFECTHEART <101324009+PERFECTHEART@users.noreply.github.com>
2023-04-21 10:57:36 +0300

    Add files via upload

--- hw1/test-int/checker.sh
+++ hw1/test-int/checker.sh
@@ -1,1 +1,32 @@
+#!/usr/bin/ksh
+# Часть строк utf8 взята отсюда: https://www.w3.org/2001/06/utf-8-test/UTF-8-demo.html
+# убраны ведущие и финишные пробелы и добавлены 4-байтные символы
 
+if [ ! -f /usr/bin/ksh ]; then
+	echo "Работа с интерпретатором, отличным от ksh не гарантируется"
+fi
+APP_TO_RUN="../build/showstats"
+DATA_FILE="test.dat"
+IFS="	"
+linenum=0
+cat ${DATA_FILE} | while read len size b1 b2 b3 b4 line; do
+	((linenum++))
+	if [ ${len} -ne 0 ]; then
+		print ${line}"Z" | ${APP_TO_RUN} | read -r rlen rsize rb1 rb2 rb3 rb4 rline
+		line="${line}Z" # Добавляем символ к строке
+		size=${#line} # Вычисляем длину строки в байтах для сравнения со значением из программы
+		# Корректируем счётчики, чтобы учесть добавление символа ASCII к строке
+		((len++))
+		((b1++))
+		for v in len b1 b2 b3 b4 line; do
+			rv=r$v
+			# Получаем значение переменной из имени, хранящемся в другой переменной
+			nameref var=${v}
+			nameref rvar=${rv}
+			if [ ${var} != ${rvar} ]; then
+				print "Файл ${DATA_FILE}, Строка #${linenum}: ожидалось значение переменной ${v} [${var}], оказалось - [${rvar}]"
+			fi
+		done
+	fi
+done
+exit 0
